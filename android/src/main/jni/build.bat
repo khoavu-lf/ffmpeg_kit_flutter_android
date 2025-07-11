d8e49cfb5b8f3140c7915eb32ce861b404bd3494
khoa-lf <khoa.dinhvu2001@lfglobaltech.com>
2025-07-11 15:44:34 +0700

    minimal ffmeg-kit download

--- android/src/main/jni/build.bat
+++ android/src/main/jni/build.bat
@@ -2,7 +2,7 @@
 setlocal
 
 rem Android AAR download URL
-set ANDROID_URL=https://github.com/sk3llo/ffmpeg_kit_flutter/releases/download/7.0/ffmpeg-kit-full-gpl-7.0.aar
+set ANDROID_URL=https://github.com/khoavu-lf/ffmpeg_kit_flutter_android/releases/download/v6.0.2/ffmpeg-kit-min-gpl-6.0-2.LTS.aar
 
 rem Create libs directory if it doesn't exist
 if not exist libs (
